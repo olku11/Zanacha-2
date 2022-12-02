4608b78fe42376bfe5bcc51a2928a9477bbeb754
olku11 <artem.chastukhin@gmail.com>
2022-12-02 22:09:19 +0300

    победаа

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtGui import QPainter, QColor, QPolygon
 import sys
 import random
+from Ui import Ui_MainWindow
 
 SCREEN_SIZE = [430, 420]
 
-class Example(QMainWindow):
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi("Ui.ui", self)
+        self.setupUi(self)
         self.flag = False
+        self.setWindowTitle('Новое окно')
         self.pushButton.clicked.connect(self.draw)
         self.coords = []
 
     def draw(self):
+        self.figure = 'circle'
         self.size = random.randint(10, 100)
-        self.color = (253, 244, 0)
+        self.color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
         self.flag = True
         self.update()
 
@@ -27,12 +32,18 @@ class Example(QMainWindow):
             qp.setPen(QColor(*self.color))
             qp.setBrush(QColor(*self.color))
             self.x, self.y = random.randint(100, SCREEN_SIZE[0] - 100), random.randint(100, SCREEN_SIZE[1] - 100)
-            qp.drawEllipse(self.x, self.y, self.size, self.size)
+            if self.figure == 'circle':
+                qp.drawEllipse(self.x, self.y, self.size, self.size)
             qp.end()
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    sys.excepthook = except_hook
     ex = Example()
     ex.show()
     sys.exit(app.exec_())
-
